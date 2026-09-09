f17c8b626d7664ac6250a0a277f6ef664a5d72d9
ubetatata-arch <ubetatata@gmail.com>
2026-09-09 20:53:12 +0530

    Create MyKeyboardService.kt

---
+++ app/app/src/main/app/src/main/java/com/deshan/keyboard/MyKeyboardService.kt
@@ -0,0 +1,28 @@
+package com.deshan.keyboard
+
+import android.inputmethodservice.InputMethodService
+import android.inputmethodservice.Keyboard
+import android.inputmethodservice.KeyboardView
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.EditorInfo
+
+class MyKeyboardService : InputMethodService(), KeyboardView.OnKeyboardActionListener {
+
+    private lateinit var keyboardView: KeyboardView
+    private lateinit var keyboard: Keyboard
+    private var isShifted = false
+
+    override fun onCreateInputView(): View {
+        keyboardView = layoutInflater.inflate(R.layout.keyboard_view, null) as KeyboardView
+        keyboard = Keyboard(this, R.xml.qwerty)
+        keyboardView.keyboard = keyboard
+        keyboardView.setOnKeyboardActionListener(this)
+        return keyboardView
+    }
+
+    override fun onStartInputView(info: EditorInfo?, restarting: Boolean) {
+        super.onStartInputView(info, restarting)
+        keyboardView.keyboard = keyboard
+    }
+    
