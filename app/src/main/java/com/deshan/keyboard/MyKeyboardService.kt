6fbfd554907c20587cf2659ef741e9882c83dadd
ubetatata-arch <ubetatata@gmail.com>
2026-09-10 10:11:36 +0530

    Update MyKeyboardService.kt

--- app/src/main/java/com/deshan/keyboard/MyKeyboardService.kt
+++ app/src/main/java/com/deshan/keyboard/MyKeyboardService.kt
@@ -10,20 +10,24 @@ import android.view.inputmethod.EditorInfo
 class MyKeyboardService : InputMethodService(), KeyboardView.OnKeyboardActionListener {
 
     private lateinit var keyboardView: KeyboardView
-    private lateinit var keyboard: Keyboard
+    private lateinit var englishKeyboard: Keyboard
+    private lateinit var sinhalaLettersKeyboard: Keyboard
+    private lateinit var sinhalaSignsKeyboard: Keyboard
     private var isShifted = false
 
     override fun onCreateInputView(): View {
         keyboardView = layoutInflater.inflate(R.layout.keyboard_view, null) as KeyboardView
-        keyboard = Keyboard(this, R.xml.qwerty)
-        keyboardView.keyboard = keyboard
+        englishKeyboard = Keyboard(this, R.xml.qwerty)
+        sinhalaLettersKeyboard = Keyboard(this, R.xml.sinhala_letters)
+        sinhalaSignsKeyboard = Keyboard(this, R.xml.sinhala_signs)
+        keyboardView.keyboard = englishKeyboard
         keyboardView.setOnKeyboardActionListener(this)
         return keyboardView
     }
 
     override fun onStartInputView(info: EditorInfo?, restarting: Boolean) {
         super.onStartInputView(info, restarting)
-        keyboardView.keyboard = keyboard
+        keyboardView.keyboard = englishKeyboard
     }
 
     override fun onKey(primaryCode: Int, keyCodes: IntArray?) {
@@ -32,7 +36,7 @@ class MyKeyboardService : InputMethodService(), KeyboardView.OnKeyboardActionLis
             Keyboard.KEYCODE_DELETE -> ic.deleteSurroundingText(1, 0)
             Keyboard.KEYCODE_SHIFT -> {
                 isShifted = !isShifted
-                keyboard.isShifted = isShifted
+                keyboardView.keyboard?.isShifted = isShifted
                 keyboardView.invalidateAllKeys()
             }
             Keyboard.KEYCODE_DONE -> ic.sendKeyEvent(
@@ -41,9 +45,13 @@ class MyKeyboardService : InputMethodService(), KeyboardView.OnKeyboardActionLis
             -2 -> {
                 // TODO: switch to a symbols/numbers keyboard layout
             }
+            -10 -> keyboardView.keyboard = sinhalaLettersKeyboard
+            -11 -> keyboardView.keyboard = englishKeyboard
+            -12 -> keyboardView.keyboard = sinhalaSignsKeyboard
+            -13 -> keyboardView.keyboard = sinhalaLettersKeyboard
             else -> {
                 var code = primaryCode.toChar()
-                if (Character.isLetter(code) && isShifted) {
+                if (Character.isLetter(code) && isShifted && keyboardView.keyboard == englishKeyboard) {
                     code = code.uppercaseChar()
                 }
                 ic.commitText(code.toString(), 1)
