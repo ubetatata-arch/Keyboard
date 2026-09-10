1e4091e5b60e205a15bfc0a91ed5571dc5e606e8
ubetatata-arch <ubetatata@gmail.com>
2026-09-10 15:15:54 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/com/deshan/keyboard/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.deshan.keyboard
+
+import android.content.Intent
+import android.os.Bundle
+import android.provider.Settings
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnEnable).setOnClickListener {
+            startActivity(Intent(Settings.ACTION_INPUT_METHOD_SETTINGS))
+        }
+
+        findViewById<Button>(R.id.btnSwitch).setOnClickListener {
+            val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showInputMethodPicker()
+        }
+    }
+}
